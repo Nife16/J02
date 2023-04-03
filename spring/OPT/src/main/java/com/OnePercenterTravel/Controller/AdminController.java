d4a0b3d27187cd489212b23f6b027406f5812d18
Nife16 <100443715+Nife16@users.noreply.github.com>
2023-04-03 15:09:58 -0500

    more stuff, added a page to create and update places, corrected ERs

--- spring/OPT/src/main/java/com/OnePercenterTravel/Controller/AdminController.java
+++ spring/OPT/src/main/java/com/OnePercenterTravel/Controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.OnePercenterTravel.Controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.OnePercenterTravel.Entity.Place;
 import com.OnePercenterTravel.Entity.Reservation;
 import com.OnePercenterTravel.Entity.User;
+import com.OnePercenterTravel.Service.PlaceService;
 import com.OnePercenterTravel.Service.UserService;
 
 @Controller
@@ -16,6 +22,8 @@ public class AdminController {
     
     @Autowired
     UserService userService;
+    @Autowired
+    PlaceService placeService;
 
     @GetMapping("/admin-tool")
     public String adminToolHome(Model model, HttpSession session) {
@@ -31,6 +39,36 @@ public class AdminController {
             return "adminTools";
         }
         return "redirect:";
+
+    }
+
+    @GetMapping("/adminPlaces")
+    public String adminPlace(Model model, HttpSession session) {
+
+        String email = (String) session.getAttribute("emailCookie");
+        if(email != null) {
+
+            User loggedInUser = userService.findUserByEmail(email);
+            model.addAttribute("user", loggedInUser);
+            if(loggedInUser.getIsAdmin() != true) {
+                return "redirect:";
+            }
+            // Model attribute for my form
+            model.addAttribute("place", new Place());
+            // Get the list of places from the DB
+            List<Place> allPlaces =  placeService.findAll();
+            model.addAttribute("allPlaces", allPlaces);
+            return "adminPlaces";
+        }
+        return "redirect:";
+    }
+
+    @PostMapping("/adminPlaces")
+    public String adminPlaces(@ModelAttribute Place place) {
+
+        placeService.save(place);
+
+        return "redirect:adminPlaces";
 
     }
 
--- spring/OPT/src/main/java/com/OnePercenterTravel/Entity/Place.java
+++ spring/OPT/src/main/java/com/OnePercenterTravel/Entity/Place.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="place")
 public class Place {
@@ -25,10 +27,13 @@ public class Place {
 	private Integer id;
     @Column(name = "name")
     private String name;
-    @Column(name = "price", scale = 2)
+    @Column(name = "price", precision = 10, scale = 2)
     private Double price;
+    @Column(name = "imgUrl")
+    private String imgUrl;
     @ManyToMany
     @JoinTable(
+        name="place_amenity",
         joinColumns=
             @JoinColumn(name="place_id", referencedColumnName="id"),
         inverseJoinColumns=
@@ -64,9 +69,15 @@ public class Place {
     }
     @Override
     public String toString() {
-        return "Place [id=" + id + ", name=" + name + ", price=" + price + ", amenities=" + amenities + "]";
+        return "Place [id=" + id + ", name=" + name + ", price=" + price + ", imgUrl=" + imgUrl + ", amenities="
+                + amenities + "]";
     }
-    
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
     
 
     
